6ef8651e496566ac104339ab9b218c8e8a861bad
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-06-19 21:30:29 +0600

    Concatenate in js

---
+++ concatenate/program2.js
@@ -0,0 +1,5 @@
+let Name = "Md Sanowar Hossen";
+let age = 30;
+let email = "sanowarwpneed@gmail.com";
+
+document.write("My name is" + name + "."+"I am "+ age + " " + "years old"+ ".");
--- number_method/program1.js
+++ number_method/program1.js
@@ -1,6 +1,6 @@
 let age = "20"; // this is the string
 let income = 500.3789; // this is the string
 console.log(typeof(age)); // check data type
-console.log(typeof(parseInt(age))); // Number type
+console.log(typeof(parseInt(age))); // String to Number type
 console.log(typeof(income));
 console.log(parseFloat(income)); // This is the floating number
