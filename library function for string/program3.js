1109418b1a182747b30767fc3813d0cd6e032ab8
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-06-19 21:44:09 +0600

    Library function for string

---
+++ library function for string/program3.js
@@ -0,0 +1,14 @@
+/** library function for string 
+ * 
+ * length, prompt
+ * 
+*/
+
+let text = "Bangladesh";
+let len = text.length;
+// document.writeln(len); // Number of character
+
+// Prompt use for user input
+let name = prompt('Enter your name :');
+let nameLen = name.length;
+document.writeln(nameLen);
