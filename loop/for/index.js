970935d517fb863ae7786742308faddfc8f2951a
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-06-23 21:56:27 +0600

    for loop

---
+++ loop/for/index.js
@@ -0,0 +1,31 @@
+// I want to show my name 10 times
+
+/**
+let i = 1;
+for(i; i<=10; i = i + 1){
+    document.writeln(`${i} = Sanowar <br/>`);
+}
+
+*/
+
+// I want to show even number
+
+let i = 2;
+for(i; i <= 100; i = i + 2){
+    document.writeln(`${i}, `);
+}
+
+document.writeln('<br/>');
+
+// Odd number
+
+let a = 1;
+for(a; a <= 100; a = a + 2){
+    document.writeln(`${a}, `);
+}
+document.writeln('<br/>');
+// last to first
+let num = 10;
+for(num; num >= 1; num = num -1){
+    document.writeln(num);
+}
