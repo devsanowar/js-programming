a95e782f7de15f585716feb6b69a4125d75ad49c
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-06-25 00:05:27 +0600

    Fizz buz challange

---
+++ loop/fizzbuz challange/index.js
@@ -0,0 +1,11 @@
+let i = 1;
+for (i; i <= 100; i++) {
+    if(i%3 == 0){
+        document.writeln(` ${i} = Sanowar <br/>`); 
+    }
+
+    if(i%5 == 0){
+        document.writeln(` ${i} = Hossen <br/>`); 
+    }
+    document.writeln(` ${i} <br/>`); 
+}
