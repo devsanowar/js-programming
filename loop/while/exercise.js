926bcb24d8e64a1e8dea2a87ee63fe411dec4939
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-06-24 07:59:20 +0600

    Updated while loop

--- loop/while/exercise.js
+++ loop/while/exercise.js
@@ -1,15 +1,34 @@
 // ১ থেকে ১০০ পর্যন্ত সংখ্যার মধ্যে যে সকল সংখ্যা ৩ এবং ৫ দিয়ে ভাগ করলে ভাগ শেষ ০ হবে তাদের যোগফল
 
 
+// let i = 1;
+// let sum = 0;
+// while (i <= 100) {
+//     if(i%3 == 0 && i%5 == 0){
+//         document.writeln(` ${i} + `);
+//         sum +=i;
+//     }
+//     i++;
+// }
+
+// document.writeln(` = ${sum}`);
+
+
 let i = 1;
 let sum = 0;
+let first = true;
+
 while (i <= 100) {
-    if(i%3 == 0 && i%5 == 0){
-        sum +=i;
+    if (i % 3 == 0 && i % 5 == 0) {
+        if (!first) {
+            document.writeln(" + ");
+        }
+        document.writeln(`${i}`);
+        sum += i;
+        first = false;
     }
     i++;
 }
 
-document.writeln(sum);
-
+document.writeln(` = ${sum}`);
 
