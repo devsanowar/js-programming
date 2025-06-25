234873b91b3ee3d860cedb5cc181126a475d4e87
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-06-25 07:00:38 +0600

    function create, function call, function parameter

---
+++ function/index.js
@@ -0,0 +1,53 @@
+// var num1 = 20;
+// var num2 = 20;
+// var sum = num1 + num2;
+// document.writeln(sum + '<br/>');
+
+
+
+// var num1 = 20;
+// var num2 = 20;
+// var sum = num1 + num2;
+// document.writeln(sum + '<br/>');
+
+
+// var num1 = 20;
+// var num2 = 20;
+// var sum = num1 + num2;
+// document.writeln(sum + '<br/>');
+
+// var num1 = 20;
+// var num2 = 20;
+// var sum = num1 + num2;
+// document.writeln(sum + '<br/>');
+
+
+
+function multiplication(num1, num2){
+let sum = num1 * num2;
+document.writeln(sum + '<br/>');
+}
+
+multiplication(10, 30);
+multiplication(40, 60);
+multiplication(30, 20);
+multiplication(33, 55);
+multiplication(55, 44);
+
+
+// value return from function
+
+function add(num1, num2){
+    let result = num1 + num2;
+    return result;
+}
+
+let result = add(20, 10);
+let result2 = add(50, 10);
+let result3 = add(50, 40);
+
+document.writeln(`${result} <br/>`);
+document.writeln(`${result2} <br/>`);
+document.writeln(`${result3} <br/>`);
+
+
