970841f1baf32c954931548ee2ff6d7bc244bff2
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-06-27 07:00:00 +0600

    Array library method push and pop

---
+++ array/array library/index.js
@@ -0,0 +1,6 @@
+let names = ['sanowar', 'rahim', 'karin', 'rubel', 'shafin', 'mobin'];
+names.push('saikat'); // Push method er maddhome array element add kora jay
+console.log(names);
+
+names.pop(); // Pop push er opposite. last element remove kora hoy pop er maddhome
+console.log(names);
