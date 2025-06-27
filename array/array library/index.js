7bf526e967c79573142ca78d7aaa37cfd639131e
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-06-27 07:09:56 +0600

    shift and unshift method

--- array/array library/index.js
+++ array/array library/index.js
@@ -1,6 +1,12 @@
 let names = ['sanowar', 'rahim', 'karin', 'rubel', 'shafin', 'mobin'];
-names.push('saikat'); // Push method er maddhome array element add kora jay
-console.log(names);
+// names.push('saikat'); // Push method er maddhome array element add kora jay
+// console.log(names);
 
-names.pop(); // Pop push er opposite. last element remove kora hoy pop er maddhome
-console.log(names);
+// names.pop(); // Pop push er opposite. last element remove kora hoy pop er maddhome
+// console.log(names);
+
+// names.shift(); // Pop er opposite method holo shift - first element remove kore fele
+// console.log(names);
+
+// names.unshift("Shanjida"); // shurute elemenet add kore
+// console.log(names);
