a7d1d0b558ab5476bb1835e65ac98b6cca96e8b5
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-06-27 16:51:45 +0600

    array splice function example

--- array/array library/index.js
+++ array/array library/index.js
@@ -11,14 +11,22 @@ let names = ['sanowar', 'rahim', 'karin', 'rubel', 'shafin', 'mobin'];
 // names.unshift("Shanjida"); // shurute elemenet add kore
 // console.log(names);
 
-let country1 = ['Bangladesh', 'Pakista', 'India', 'Srilanka'];
-let country2 = ['Nepal', 'Maldiv', 'Vhutan'];
+// let country1 = ['Bangladesh', 'Pakista', 'India', 'Srilanka'];
+// let country2 = ['Nepal', 'Maldiv', 'Vhutan'];
 
-let country = country1.concat(country2); // দুটি অ্যারে একসাথে যোগ করে
-console.log(country);
+// let country = country1.concat(country2); // দুটি অ্যারে একসাথে যোগ করে
+// console.log(country);
 
-console.log(names);
+// console.log(names);
 // let arrayLength = names.length;
 // console.log(arrayLength);
-// let sliced = names.slice(1, 4);
+// let sliced = names.slice(1, 4);   //নির্দিষ্ট অংশ কেটে নেয় (মূল অ্যারে অপরিবর্তিত থাকে)
 // console.log(sliced);
+
+
+// splice(start, deleteCount, newItems...)  - Array থেকে ডাটা ডিলিট করে বা নতুন ডাটা অ্যাড করে
+
+let numbers = [1,2,3,4,5,6];
+// numbers.splice(1, 3); // জাস্ট ডাটা ডিলিট 
+numbers.splice(1, 3, 20, 15); // ডাটা ডিলিট করেছে এবং নতুন ডাটা অ্যাড করেছে
+console.log(numbers);
