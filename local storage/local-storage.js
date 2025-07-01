f0e4dff208ca4aa25ed3726f5ac5eb950cdd8f7f
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-07-02 05:28:50 +0600

    storage api js (LocalStorage)

---
+++ local storage/local-storage.js
@@ -0,0 +1,24 @@
+// localStorage.setItem("userName", "devsanowar"); // Set data in local storage
+// localStorage.removeItem("userName"); // local storage data remove
+
+// localStorage.setItem("userName", "Md Sanowar Hossen");
+// localStorage.setItem("password", "12345678");
+
+// const userName = localStorage.getItem("userName");
+// const userPassword = localStorage.getItem("password");
+// console.log(userName, userPassword);
+
+// localStorage.clear();
+
+
+// const countries = ["Bangladesh", "Nepal", "Vhutan"];
+// localStorage.setItem("countries", JSON.stringify(countries));
+// const getCountries = JSON.parse(localStorage.getItem("countries"));
+// console.log(getCountries);
+
+// localStorage.clear();
+
+
+// const countries = ["Bangladesh", "Nepal", "Vhutan"];
+// localStorage.setItem("countries", JSON.stringify(countries));
+// localStorage.removeItem("countries");
