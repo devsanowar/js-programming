6ad59aa15b7a2d0deb3704b9a29f7ea0318d8e4a
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-07-01 08:16:05 +0600

    json path, data modify and delete

---
+++ json/example2/index.js
@@ -0,0 +1,16 @@
+const data = require('./friends_data.json');
+// console.log(data[0].name);
+// console.log(data[0].age);
+// console.log(data[1].name);
+// console.log(data[1].age);
+
+
+
+// console.log(data[0]);
+// data[0].name = "Saikat";
+// console.log(data[0]);
+
+
+// console.log(data[0]);
+// delete data[0].name;
+// console.log(data[0]);
