ac718589ab1e2f6e44d37717518ca5313b5ced86
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-07-02 05:53:38 +0600

    sessionStorage

---
+++ local storage/session-storage.js
@@ -0,0 +1,29 @@
+// localStorage vs sessionStorage
+// 10mb vs 5mb
+// permanant vs session (tab) browser off holei session clear hoye jay
+
+// sessionStorage.setItem("userName", "devsanowar");
+// sessionStorage.setItem("password", "12345678");
+
+// const userName = sessionStorage.getItem("userName");
+// const userPassword = sessionStorage.getItem("password");
+
+// sessionStorage.removeItem("userName");
+// sessionStorage.removeItem("password");
+
+// console.log(userName, userPassword);
+
+// localStorage.clear();
+
+// const students = {
+//     id : 101,
+//     name : "Md Sanowar Hossen",
+//     class : 10,
+//     age : 30,
+//     phone : "01786998636",
+// }
+// sessionStorage.setItem("students", JSON.stringify(students));
+
+// const getStudent = JSON.parse(sessionStorage.getItem("students"));
+// console.log(getStudent);
+
