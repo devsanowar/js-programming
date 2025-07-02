46ee4319fa6f0eece984ba7f7c89487d711d52b2
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-07-02 23:05:23 +0600

    new element add and design parant div container. find element and add text then text style updated

--- Js DOM event handler/practice-one/practice.js
+++ Js DOM event handler/practice-one/practice.js
@@ -18,3 +18,45 @@ link.addEventListener('mouseleave', function() {
     link.style.borderColor = "red";
 });
 
+
+// style for my-div section
+let headingOne = document.getElementsByTagName('h1')[0];
+let text = document.createTextNode("Our Services");
+headingOne.appendChild(text);
+
+let headingFour = document.getElementsByTagName("h4")[0];
+let text2 = document.createTextNode("This is our most valuable services");
+headingFour.appendChild(text2);
+
+
+
+let myDiv = document.querySelector('.my-div');
+myDiv.style.backgroundColor = "green";
+myDiv.style.width = "400px";
+myDiv.style.height = "400px";
+myDiv.style.display = "block";
+myDiv.style.textAlign = "center";
+myDiv.style.padding = "20px"
+myDiv.style.boxSizing = "border-box"
+myDiv.style.margin = "auto";
+
+
+let paragraph = document.createElement("p");
+let textPara = document.createTextNode("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+paragraph.appendChild(textPara);
+
+myDiv.appendChild(paragraph);
+
+
+headingOne.style.color = "white";
+headingOne.style.fontSize = "24px";
+headingOne.style.textTransform = "Uppercase";
+headingOne.style.fontFamily = "roboto";
+headingOne.style.fontWeight = "700";
+
+
+headingFour.style.color = "#f2f2f2";
+headingFour.style.fontSize = "18px";
+
+paragraph.style.color ="#fff";
+
