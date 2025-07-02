aa7fcfb2364604c26ba0f2920322e59df5c8a747
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-07-02 20:40:03 +0600

    light on off function done

---
+++ Js DOM event handler/light on-off/custom.js
@@ -0,0 +1,9 @@
+
+
+function lightOff(){
+    document.querySelector('#light-on-off').src = "image/off.jpg";
+}
+
+function lightOn(){
+    document.querySelector('#light-on-off').src = "image/on.jpg";
+}
