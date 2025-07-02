ad2e2a8296b39675176752f5643f54a0a4c832b1
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-07-02 06:49:25 +0600

    dom html element select

---
+++ JS DOM (Document Object Model)/select.js
@@ -0,0 +1,8 @@
+const demo = document.getElementById('demo');
+demo.innerHTML = "Welcome to our script world";
+
+document.getElementById('page-title').innerHTML="Our Services";
+
+// document.getElementsByTagName('h1').innerHTML="h2";
+
+document.getElementsByClassName('para')[0].innerHTML = "Our service is a good service compared to other company services."; //
