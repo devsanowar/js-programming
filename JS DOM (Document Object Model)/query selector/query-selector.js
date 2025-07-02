56eb6b95dfc82423f20e835300f22f5f8d87c64e
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-07-02 07:31:44 +0600

    querySelector and querySelectorAll

---
+++ JS DOM (Document Object Model)/query selector/query-selector.js
@@ -0,0 +1,7 @@
+// document.querySelector('#title').innerHTML="I am fine. Thank you!";
+// document.querySelector('.title').innerHTML="Hey, What are you doing?";
+// document.querySelector('p').innerHTML="Hey, I am working?";
+
+
+document.querySelector('.menu ul li a').innerHTML = "Our Services";
+document.querySelectorAll('h2')[3].innerHTML = "custom-heading";
