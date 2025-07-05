662fbd68a2b5ae6c2a2ad17230b07dd3586ed44f
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-07-05 09:45:27 +0600

    Minor update

--- Js DOM event handler/style change dynamically/custom.js
+++ Js DOM event handler/style change dynamically/custom.js
@@ -1,13 +1,12 @@
+let StyleId = document.querySelector("#paraId");
 function addStyle(){
-    let addStyleId = document.querySelector("#paraId");
     // addStyleId.style.color ="red";
     // addStyleId.style.fontSize ="3rem";
     // addStyleId.style.fontWeight ="bold";
     // addStyleId.style.fontStyle ="italic";
-    addStyleId.classList.add("para-style");
+    StyleId.classList.add("para-style");
 }
 
 function removeStyle(){
-    let removeStyle = document.querySelector("#paraId");
-    removeStyle.classList.remove("para-style");
+    StyleId.classList.remove("para-style");
 }
