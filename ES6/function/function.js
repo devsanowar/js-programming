6d91adf37f051dec58dcf1929c4d2dd1a8fb0b90
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-07-15 21:57:59 +0600

    function practice

---
+++ ES6/function/function.js
@@ -0,0 +1,26 @@
+// reguler function
+// function add(a, b){
+//     return a + b;
+// }
+
+//  let sum = add(5,7);
+// console.log(sum);
+
+// multiplication = (number1, number2) => number1 * number2;
+// let mult = multiplication(10, 10);
+// console.log(mult);
+
+// default parameter (Reguler function)
+// ---------------------------------------
+
+// function personName(name = "Sanowar"){
+//     return`Hello ${name}`;
+// }
+
+// console.log(personName());
+// console.log(personName('Karim'));
+
+// default parameter (ES6 function)
+//  perName = (name = "Md Sanowar Hossen") =>  `Hello ${name}`;
+
+//  console.log(perName());
