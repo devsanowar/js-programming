30ef3a096e8c2ae7501b000f5737b8c06f25d05b
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-07-16 05:16:03 +0600

    ES6 function expression in object

--- ES6/function/function.js
+++ ES6/function/function.js
@@ -24,3 +24,44 @@
 //  perName = (name = "Md Sanowar Hossen") =>  `Hello ${name}`;
 
 //  console.log(perName());
+
+// const person = {
+//     name: "Md Sanowar Hossen",
+//     age: 30,
+//     phone: "01786998635",
+//     email: "sanowargiit22@gmail.com",
+//     edu: function(){
+//         return "Ghatail gono pilot high school";
+//     }
+// }
+
+// console.log(person.edu());
+
+// const person = {
+//     name: "Md Sanowar Hossen",
+//     age: 30,
+//     phone: "01786998635",
+//     email: "sanowargiit22@gmail.com",
+//     edu: function(){
+//         return {
+//             instituteName : "Ghatail cant public school and collage",
+//         }
+//     }
+// }
+
+// console.log(person.edu().instituteName);
+
+
+const person = {
+    name: "Md Sanowar Hossen",
+    age: 30,
+    phone: "01786998635",
+    email: "sanowargiit22@gmail.com",
+    edu: () => {
+        return {
+            instituteName : "Ghatail cant public school and collage",
+        }
+    }
+}
+
+console.log(person.edu().instituteName);
