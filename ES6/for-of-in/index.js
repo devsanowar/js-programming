1cef04610bfe7f0b8ad0b4e774b3a134d63c13e8
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-07-16 05:39:16 +0600

    for in and for of practice

---
+++ ES6/for-of-in/index.js
@@ -0,0 +1,18 @@
+// For of = use for array
+
+const numbers = [1,5,2,4,5,3];
+for(number of numbers){
+    console.log(number);
+}
+
+// For in = User for object
+
+const Student ={
+    ID : 101,
+    name : 'Md Sanowar Hossen',
+    cgpa : 3.21
+}
+
+for(x in Student){
+    console.log(`${x} : ${Student[x]}`);
+}
