692289334a8cc2f6da52911b0e526868a81c1c38
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-07-15 20:35:18 +0600

    variable example done

---
+++ ES6/variable/variable.js
@@ -0,0 +1,25 @@
+// var name = "sanowar";
+// name = "Hossen";
+// var name = "Md Sanwoar Hossen"
+
+// function fullName(){
+//     var name = "MD SANO";
+//     console.log(name);
+// }
+
+// fullName();
+
+// console.log(name);
+
+//let name = "Md Sanowar";
+//name = "Hossen"; // reassign kora jay
+// let name = "MD"; // redeclare kora jay na
+//console.log(name);
+
+const numbers = [1,2,3,4,7];
+// numbers = [5,6]; // Reassign possible na
+ //const numbers = [4,5]; // Redeclare possible na
+
+ numbers.push(5,6); // push kora possible
+ 
+console.log(numbers);
